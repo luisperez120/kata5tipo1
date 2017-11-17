871fd1a3070c5046b6c135ee5f42a15d68199dc0
Luis Pérez <Luis Prez@ToshibaLPM>
2017-11-17 14:15:19 +0000

    Commit 2: Inserta 260 correos

--- src/kata5tipo1/Kata5Tipo1.java
+++ src/kata5tipo1/Kata5Tipo1.java
@@ -5,6 +5,11 @@
  */
 package kata5tipo1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -21,7 +26,7 @@ public class Kata5Tipo1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, FileNotFoundException, IOException {
         
         
         Class.forName("org.sqlite.JDBC");
@@ -32,9 +37,30 @@ public class Kata5Tipo1 {
         String query ="Select * FROM PEOPLE";
         ResultSet rs= st.executeQuery(query);
         while(rs.next()){
-            System.out.println(rs.getInt(1));
-            System.out.println(rs.getInt(2));
+           //System.out.println(rs.getInt(1));
+           // System.out.println(rs.getInt(2));
         }
+         
+            
+        query = " Create table if not exists mail ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'Mail' Text Not null);";
+        st.execute(query);
+
+        
+        BufferedReader br = new BufferedReader(new FileReader(new File("mail.txt")));
+        String linea;
+            //System.out.println(linea);
+            while((linea=br.readLine())!=null){
+                if(!linea.contains("@")){
+                    continue;
+                }
+                //System.out.println(linea);
+                query = "insert into mail (Mail) Values ('" + linea +"');";
+                st.executeUpdate(query);
+            }
+            
+            
+            query= "Select count(*) from mail";
+            System.out.println(st.executeQuery(query).getInt(1));
     }
     
 }
